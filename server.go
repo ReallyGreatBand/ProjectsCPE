a6868441d2c63321cf6e55391247515b2f903f7d
Hud-Volodymyr <v.gud2001@gmail.com>
2020-09-08 17:57:09 +0300

    upload server code

--- server.go
+++ server.go
@@ -1,3 +1,35 @@
-package ProjectsCPE
+package main
 
+import (
+"encoding/json"
+"log"
+"net/http"
+"time"
+)
+
+type Session struct {
+	Time    string     `json:"time"`
+}
+
+func MustEncode(enc *json.Encoder, v interface{}) {
+	err := enc.Encode(v)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func jsonHandler(w http.ResponseWriter, _ *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	t:= time.Now().Format(time.RFC3339)
+	showTime := Session {
+		Time: t,
+	}
+	MustEncode(json.NewEncoder(w), showTime)
+}
+
+func main() {
+	http.HandleFunc("/time", jsonHandler)
+	log.Fatal(http.ListenAndServe(":8795", nil))
+}
 
