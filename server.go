3ffc10472b00a1bb511260f5bcaeee794903db0a
Владислав Кучин <vladislavkuchin2001@gmail.com>
2020-09-08 18:07:27 +0300

    fixed empty interface

--- server.go
+++ server.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-"encoding/json"
-"log"
-"net/http"
-"time"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
 )
 
 type Session struct {
-	Time    string     `json:"time"`
+	Time string `json:"time"`
 }
 
-func MustEncode(enc *json.Encoder, v interface{}) {
+func MustEncode(enc *json.Encoder, v Session) {
 	err := enc.Encode(v)
 	if err != nil {
 		panic(err)
@@ -21,8 +21,8 @@ func MustEncode(enc *json.Encoder, v interface{}) {
 func jsonHandler(w http.ResponseWriter, _ *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
-	t:= time.Now().Format(time.RFC3339)
-	showTime := Session {
+	t := time.Now().Format(time.RFC3339)
+	showTime := Session{
 		Time: t,
 	}
 	MustEncode(json.NewEncoder(w), showTime)
@@ -32,4 +32,3 @@ func main() {
 	http.HandleFunc("/time", jsonHandler)
 	log.Fatal(http.ListenAndServe(":8795", nil))
 }
-
